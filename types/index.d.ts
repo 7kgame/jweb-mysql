f5433f5860e48c4e631345fe90d89d1df4ec80e4
cyij <cyij2006@gmail.com>
2019-04-08 11:09:32 +0800

    getClient return Poll

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,6 +2,6 @@ import { createPool, Pool, PoolConnection } from 'mysql'
 
 export default interface MysqlDao {
   connect (): Promise<PoolConnection>
-  getClient (): PoolConnection
+  getClient (): Pool
   disconnect (): Promise<void>
 }
